e2c91d333f6ebafe5de033657ee246c893a36201
Bernardo-R <brosario12@live.com>
2023-04-27 16:23:12 -0400

    My first React App and class Assignments

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Nav from "./nav";
+import LogInForm from "./loginForm";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <LogInForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="pt-3 mt-4 text-muted border-top">©copyright 2023</footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
